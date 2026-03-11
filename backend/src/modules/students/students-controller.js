4498b9b66b3bfd829f193ee5b68edb3ce1a0cdc7
rohanfaldu <faldurohan1991@gmail.com>
2026-03-11 15:29:05 +0530

    resolve frontend and backend issue

--- backend/src/modules/students/students-controller.js
+++ backend/src/modules/students/students-controller.js
@@ -2,28 +2,43 @@ const asyncHandler = require("express-async-handler");
 const { getAllStudents, addNewStudent, getStudentDetail, setStudentStatus, updateStudent } = require("./students-service");
 
 const handleGetAllStudents = asyncHandler(async (req, res) => {
-    //write your code
-
+    const { name, class: className, section, roll } = req.query;
+    const students = await getAllStudents({ name, className, section, roll });
+    res.json({ students });
 });
 
 const handleAddStudent = asyncHandler(async (req, res) => {
-    //write your code
-
+    const payload = req.body;
+    const message = await addNewStudent(payload);
+    res.json(message);
 });
 
 const handleUpdateStudent = asyncHandler(async (req, res) => {
-    //write your code
-
+    const { id: userId } = req.params;
+    const payload = req.body;
+    const message = await updateStudent({ ...payload, userId });
+    res.json(message);
 });
 
 const handleGetStudentDetail = asyncHandler(async (req, res) => {
-    //write your code
-
+    const { id } = req.params;
+    const student = await getStudentDetail(id);
+    res.json(student);
 });
 
 const handleStudentStatus = asyncHandler(async (req, res) => {
-    //write your code
+    const { id: userId } = req.params;
+    const { status } = req.body;
+    const { id: reviewerId } = req.user;
+    const message = await setStudentStatus({ userId, reviewerId, status });
+    res.json(message);
+});
 
+const handleDeleteStudent = asyncHandler(async (req, res) => {
+    const { id: userId } = req.params;
+    const { id: reviewerId } = req.user;
+    const message = await setStudentStatus({ userId, reviewerId, status: false });
+    res.json(message);
 });
 
 module.exports = {
@@ -32,4 +47,5 @@ module.exports = {
     handleAddStudent,
     handleStudentStatus,
     handleUpdateStudent,
+    handleDeleteStudent,
 };
